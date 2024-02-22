0a33a978806b45d49bbb0d34445b06f0dfb7c2a4
Fizza <alisyedfizza@gmail.com>
2024-02-21 18:09:50 -0600

    added movies to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,9 +16,15 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Madam Web', :rating => 'PG-13',
+  :release_date => '14-Feb-2024'},
+  {:title => 'Wonka', :rating => 'PG-13',
+  :release_date => '13-Jan-2024'},
+  {:title => 'Trolls Band Together', :rating => 'PG-13',
+  :release_date => '02-Jan-2024'}
 ]
 
 more_movies.each do |movie|
-  Movie.create!(movie)
+  Movie.find_or_create_by!(movie)
 end
